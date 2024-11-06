cb3a7e16e4d4dbd458ce984fb223e3c6d37fc050
gou <19513317149@163.com>
2024-11-06 19:59:23 +0800

    在main分支上成功运行max功能，进行进一步提交

--- text_many/main.cpp
+++ text_many/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "swap.h"
+#include "max.h"
 
 using namespace std;
 
@@ -7,6 +8,8 @@ int main (int argc, char **argv)
 {
     int val1 = 10;
     int val2 = 20;
+    int the_max_val;
+
     cout << "Before swap:" << endl;
     cout << "val1 = "<<val1 << endl;
     cout << "val2 = "<<val2 << endl;
@@ -16,5 +19,9 @@ int main (int argc, char **argv)
     cout << "After swap:" << endl;
     cout << "val1 = "<<val1 << endl;
     cout << "val2 = "<<val2 << endl;
+
+    the_max_val = max(val1,val2);
+    cout << "the_max_val =  "<<the_max_val << endl;
+    
     return 0;
 }
