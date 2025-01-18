f0caf34070c0698bff71332541bd6509743abc12
gou <19513317149@163.com>
2025-01-18 21:19:19 +0800

    寒假的第一次提交-将单个功能部分加入最大值比较

--- text_one/main.cpp
+++ text_one/main.cpp
@@ -2,13 +2,31 @@
 
 using namespace std;
 
-void swap (int &a,int &b)
+void swap (int &a,int &b)       //交换功能
 {
     int temp;
     temp = a;
     a = b;
     b = temp;
 }
+
+int max(int &num1, int &num2)    //选择最大值功能
+{
+    int max;
+    if (num1 > num2) 
+    {
+        max = num1;
+    } 
+    else
+    {
+        max = num2;
+    }
+    return max;     //返回最大值
+}
+
+//常量区域
+ int max_one;
+
 int main (int argc, char **argv)
 {
     int val1 = 10;
@@ -18,9 +36,11 @@ int main (int argc, char **argv)
     cout << "val2 = "<<val2 << endl;
 
     swap(val1, val2);
+    max_one  = max(val1, val2);
 
     cout << "After swap:" << endl;
     cout << "val1 = "<<val1 << endl;
     cout << "val2 = "<<val2 << endl;
+    cout << "max =  " << max_one << endl;
     return 0;
 }
